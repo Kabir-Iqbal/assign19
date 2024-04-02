daff06350b8454f5674ece44b541d25f6e196277
Kabir-iqbal <161919126+Kabir-Iqbal@users.noreply.github.com>
2024-04-03 03:08:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+// 19. Dinner Guests: 
+// Working with one of the programs from Exercises 14 through 18, print a message indicating 
+// the number of people you are inviting to dinner.
+let invitation = ["Ali", "Umair", "Amin", "Uzair", "Rauf"];
+invitation.forEach(guest => {
+    console.log(`Hello ${invitation} you are invited for dinner`);
+});
---
+++ main.ts
@@ -0,0 +1,8 @@
+// 19. Dinner Guests: 
+// Working with one of the programs from Exercises 14 through 18, print a message indicating 
+// the number of people you are inviting to dinner.
+let invitation : string []= ["Ali","Umair","Amin","Uzair","Rauf"]
+invitation.forEach(guest => {
+    console.log(`Hello ${invitation} you are invited for dinner`);
+    
+});
